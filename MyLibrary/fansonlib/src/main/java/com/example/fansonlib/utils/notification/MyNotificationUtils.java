633f4f9fcb8e39beca5b04f63f136fe32c31d3ed
DESKTOP-JNRV3US\fanson <fansonq@gmaiil.com>
2017-06-15 10:13:43 +0800

    MyNotificationUtils新增传参方法

--- MyLibrary/fansonlib/src/main/java/com/example/fansonlib/utils/notification/MyNotificationUtils.java
+++ MyLibrary/fansonlib/src/main/java/com/example/fansonlib/utils/notification/MyNotificationUtils.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 import com.example.fansonlib.utils.notification.builder.BigPicBuilder;
 import com.example.fansonlib.utils.notification.builder.BigTextBuilder;
@@ -22,21 +23,23 @@ import com.example.fansonlib.utils.notification.builder.SingleLineBuilder;
 
 public class MyNotificationUtils {
 
-    public static  Context mContext;
+    public static Context mContext;
 
-    private static  NotificationManager mNotificationManager;
+    private static NotificationManager mNotificationManager;
 
     /**
      * 初始化
+     *
      * @param context 传入的必须是Application
      */
-    public static void init(Context context){
+    public static void init(Context context) {
         mContext = context;
-        mNotificationManager = (NotificationManager)context.getSystemService(Activity.NOTIFICATION_SERVICE);
+        mNotificationManager = (NotificationManager) context.getSystemService(Activity.NOTIFICATION_SERVICE);
     }
 
     /**
      * 单行，普通的建造
+     *
      * @param id
      * @param smallIcon
      * @param contentTitle
@@ -44,9 +47,9 @@ public class MyNotificationUtils {
      * @param contentIntent
      * @return
      */
-    public static SingleLineBuilder buildSimple(int id,int smallIcon,CharSequence contentTitle ,CharSequence contentText,PendingIntent contentIntent){
+    public static SingleLineBuilder buildSimple(int id, int smallIcon, CharSequence contentTitle, CharSequence contentText, PendingIntent contentIntent) {
         SingleLineBuilder builder = new SingleLineBuilder();
-        builder.setBase(smallIcon,contentTitle,contentText)
+        builder.setBase(smallIcon, contentTitle, contentText)
                 .setId(id)
                 .setContentIntent(contentIntent);
         return builder;
@@ -54,6 +57,7 @@ public class MyNotificationUtils {
 
     /**
      * 带进度条的建造
+     *
      * @param id
      * @param smallIcon
      * @param contentTitle
@@ -61,16 +65,17 @@ public class MyNotificationUtils {
      * @param max
      * @return
      */
-    public static ProgressBuilder buildProgress(int id, int smallIcon, CharSequence contentTitle, int progress, int max){
+    public static ProgressBuilder buildProgress(int id, int smallIcon, CharSequence contentTitle, int progress, int max) {
         ProgressBuilder builder = new ProgressBuilder();
-        builder.setBase(smallIcon,contentTitle,progress+"/"+max)
+        builder.setBase(smallIcon, contentTitle, progress + "/" + max)
                 .setId(id);
-        builder.setProgress(max,progress,false);
+        builder.setProgress(max, progress, false);
         return builder;
     }
 
     /**
      * 带图片的建造
+     *
      * @param id
      * @param smallIcon
      * @param contentTitle
@@ -78,42 +83,15 @@ public class MyNotificationUtils {
      * @param summaryText
      * @return
      */
-    public static BigPicBuilder buildBigPic(int id, int smallIcon, CharSequence contentTitle, CharSequence contentText, CharSequence summaryText){
+    public static BigPicBuilder buildBigPic(int id, int smallIcon, CharSequence contentTitle, CharSequence contentText, CharSequence summaryText) {
         BigPicBuilder builder = new BigPicBuilder();
-        builder.setBase(smallIcon,contentTitle,contentText).setId(id);
+        builder.setBase(smallIcon, contentTitle, contentText).setId(id);
         builder.setSummaryText(summaryText);
         return builder;
     }
 
     /**
      * 长文的建造
-     * @param id
-     * @param smallIcon
-     * @param contentTitle
-     * @param contentText
-     * @return
-     */
-    public static BigTextBuilder buildBigText(int id, int smallIcon, CharSequence contentTitle, CharSequence contentText){
-        BigTextBuilder builder = new BigTextBuilder();
-        builder.setBase(smallIcon,contentTitle,contentText).setId(id);
-        //builder.setSummaryText(summaryText);
-        return builder;
-    }
-
-    /**
-     * 多条信息的建造
-     * @param id
-     * @param smallIcon
-     * @param contentTitle
-     * @return
-     */
-    public static MailboxBuilder buildMailBox(int id, int smallIcon, CharSequence contentTitle){
-        MailboxBuilder builder = new MailboxBuilder();
-        builder.setBase(smallIcon,contentTitle,"").setId(id);
-        return builder;
-    }
-
-    /**
      *
      * @param id
      * @param smallIcon
@@ -121,40 +99,82 @@ public class MyNotificationUtils {
      * @param contentText
      * @return
      */
-    public static MediaBuilder buildMedia(int id, int smallIcon, CharSequence contentTitle, CharSequence contentText){
+    public static BigTextBuilder buildBigText(int id, int smallIcon, CharSequence contentTitle, CharSequence contentText) {
+        BigTextBuilder builder = new BigTextBuilder();
+        builder.setBase(smallIcon, contentTitle, contentText).setId(id);
+        //builder.setSummaryText(summaryText);
+        return builder;
+    }
+
+    /**
+     * 多条信息的建造
+     *
+     * @param id
+     * @param smallIcon
+     * @param contentTitle
+     * @return
+     */
+    public static MailboxBuilder buildMailBox(int id, int smallIcon, CharSequence contentTitle) {
+        MailboxBuilder builder = new MailboxBuilder();
+        builder.setBase(smallIcon, contentTitle, "").setId(id);
+        return builder;
+    }
+
+    /**
+     * @param id
+     * @param smallIcon
+     * @param contentTitle
+     * @param contentText
+     * @return
+     */
+    public static MediaBuilder buildMedia(int id, int smallIcon, CharSequence contentTitle, CharSequence contentText) {
         MediaBuilder builder = new MediaBuilder();
-        builder.setBase(smallIcon,contentTitle,contentText).setId(id);
+        builder.setBase(smallIcon, contentTitle, contentText).setId(id);
         return builder;
     }
 
     /**
      * 建造PendingIntent
-     * @param clazz
+     *
+     * @param clazz 指定跳转的类
      * @return
      */
-    public static PendingIntent buildIntent(Class clazz){
+    public static PendingIntent buildIntent(Class clazz) {
+        return buildIntentWithBundle(clazz, null);
+    }
+
+    /**
+     * 建造PendingIntent带参数
+     *
+     * @param clazz 指定跳转的类
+     * @param bundle 参数
+     * @return
+     */
+    public static PendingIntent buildIntentWithBundle(Class clazz, Bundle bundle) {
         int flags = PendingIntent.FLAG_UPDATE_CURRENT;
         Intent intent = new Intent(MyNotificationUtils.mContext, clazz);
-        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_NEW_TASK);
-        PendingIntent pi = PendingIntent.getActivity(MyNotificationUtils.mContext, 0, intent, flags);
-        return pi;
+        intent.putExtras(bundle);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        return PendingIntent.getActivity(MyNotificationUtils.mContext, 0, intent, flags);
     }
 
     /**
      * 开启通知
+     *
      * @param id
      * @param notification
      */
-    public static void notify(int id,Notification notification){
-        mNotificationManager.notify(id,notification);
+    public static void notify(int id, Notification notification) {
+        mNotificationManager.notify(id, notification);
     }
 
     /**
      * 移除指定通知
+     *
      * @param id ID
      */
-    public static void cancel(int id){
-        if(mNotificationManager!=null){
+    public static void cancel(int id) {
+        if (mNotificationManager != null) {
             mNotificationManager.cancel(id);
         }
     }
@@ -162,8 +182,8 @@ public class MyNotificationUtils {
     /**
      * 移除所有通知
      */
-    public static void cancelAll(){
-        if(mNotificationManager!=null){
+    public static void cancelAll() {
+        if (mNotificationManager != null) {
             mNotificationManager.cancelAll();
         }
     }
