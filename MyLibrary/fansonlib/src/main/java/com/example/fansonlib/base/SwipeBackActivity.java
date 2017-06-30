21fc0d1ce4e63b3e7260f99828339b057c00d18d
DESKTOP-JNRV3US\fanson <fansonq@gmail.com>
2017-06-30 10:51:56 +0800

    修改SwipeBack；

--- MyLibrary/fansonlib/src/main/java/com/example/fansonlib/base/SwipeBackActivity.java
+++ MyLibrary/fansonlib/src/main/java/com/example/fansonlib/base/SwipeBackActivity.java
@@ -15,15 +15,27 @@ public abstract class SwipeBackActivity extends BaseActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SwipeBackHelper.onCreate(this);
-//        SwipeBackHelper.getCurrentPage(this)
-//                .setSwipeBackEnable(true)
-//                .setSwipeEdgePercent(0.2f)//0.2 mean left 20% of screen can touch to begin swipe.
-//                .setSwipeSensitivity(1)//sensitiveness of the gesture。0:slow  1:sensitive
-//                .setScrimColor(Color.TRANSPARENT)//color of Scrim below the activity
-//                .setClosePercent(0.8f)//close activity when swipe over this
-//                .setSwipeRelateEnable(true)//if should move together with the following Activity
-//                .setSwipeRelateOffset(500)//the Offset of following Activity when setSwipeRelateEnable(true)
-//                .setDisallowInterceptTouchEvent(true);
+//        SwipeBackHelper.getCurrentPage(this)//获取当前页面
+//                .setSwipeBackEnable(true)//设置是否可滑动
+//                .setSwipeEdge(200)//可滑动的范围。px。200表示为左边200px的屏幕
+//                .setSwipeEdgePercent(0.2f)//可滑动的范围。百分比。0.2表示为左边20%的屏幕
+//                .setSwipeSensitivity(0.5f)//对横向滑动手势的敏感程度。0为迟钝 1为敏感
+//                .setScrimColor(Color.BLUE)//底层阴影颜色
+//                .setClosePercent(0.8f)//触发关闭Activity百分比
+//                .setSwipeRelateEnable(false)//是否与下一级activity联动(微信效果)。默认关
+//                .setSwipeRelateOffset(500)//activity联动时的偏移量。默认500px。
+//                .setDisallowInterceptTouchEvent(true)//不抢占事件，默认关（事件将先由子View处理再由滑动关闭处理）
+//                .addListener(new SwipeListener() {//滑动监听
+//                    @Override
+//                    public void onScroll(float percent, int px) {//滑动的百分比与距离
+//                    }
+//                    @Override
+//                    public void onEdgeTouch() {//当开始滑动
+//                    }
+//                    @Override
+//                    public void onScrollToClose() {//当滑动关闭
+//                    }
+//                });
 
         SwipeBackHelper.getCurrentPage(this)
                 .setSwipeBackEnable(true)
