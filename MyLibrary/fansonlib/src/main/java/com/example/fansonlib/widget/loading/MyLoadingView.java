754e984657f300167973a8db7dd3ef69e4f73b50
company\fanson <fansonq@gmail.com>
2017-08-03 17:20:57 +0800

    更改LoadingView

--- MyLibrary/fansonlib/src/main/java/com/example/fansonlib/widget/loading/MyLoadingView.java
+++ MyLibrary/fansonlib/src/main/java/com/example/fansonlib/widget/loading/MyLoadingView.java
@@ -17,7 +17,7 @@ import com.example.fansonlib.R;
  * Created on：2016/12/17 10:34
  * Describe：
  */
-public class MyLoadingView extends RelativeLayout  implements View.OnClickListener {
+public class MyLoadingView extends RelativeLayout implements View.OnClickListener {
 
     /**
      * 默认提示TextView
@@ -65,12 +65,12 @@ public class MyLoadingView extends RelativeLayout  implements View.OnClickListen
 
     public MyLoadingView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context,attrs);
+        init(context, attrs);
     }
 
     public MyLoadingView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context,attrs);
+        init(context, attrs);
     }
 
     private void init(Context context, AttributeSet attrs) {
@@ -80,15 +80,15 @@ public class MyLoadingView extends RelativeLayout  implements View.OnClickListen
         mLoadingText = typedArray.getString(R.styleable.MyLoadingView_empty_loading_txt);
         typedArray.recycle();
 
-        if(TextUtils.isEmpty(mWarnText)){
+        if (TextUtils.isEmpty(mWarnText)) {
             mWarnText = "暂无数据...";
         }
 
-        if(TextUtils.isEmpty(buttonText)){
+        if (TextUtils.isEmpty(buttonText)) {
             buttonText = "重新加载";
         }
 
-        if(TextUtils.isEmpty(mLoadingText)){
+        if (TextUtils.isEmpty(mLoadingText)) {
             mLoadingText = "加载中...";
         }
 
@@ -109,7 +109,7 @@ public class MyLoadingView extends RelativeLayout  implements View.OnClickListen
         mWarnLp.addRule(RelativeLayout.CENTER_IN_PARENT);
         mWarnLp.addRule(RelativeLayout.ABOVE, R.id.id_empty_btn_view);
         mWarnView.setId(R.id.id_empty_tv_view);
-        addView(mWarnView,mWarnLp);
+        addView(mWarnView, mWarnLp);
 
         mProgressDialog = new ProgressAlertDialog(getContext());
 
@@ -121,25 +121,27 @@ public class MyLoadingView extends RelativeLayout  implements View.OnClickListen
     /**
      * 加载中
      */
-    public void loading(){
-        if(mBindView!=null){
+    public void loading() {
+        if (mBindView != null) {
             mBindView.setVisibility(GONE);
         }
-        if(mLoadingModel == MODEL_DEFAULT){
+        if (mLoadingModel == MODEL_DEFAULT) {
             setVisibility(VISIBLE);
             mLoadDataBtn.setVisibility(INVISIBLE);
-            if(!hasCustomLoadingView) {
+            if (!hasCustomLoadingView) {
                 mWarnView.setText(mLoadingText);
-            }else{
-                if(mCustomLoadingView!=null){
+            } else {
+                if (mCustomLoadingView != null) {
                     mWarnView.setVisibility(GONE);
                     mCustomLoadingView.setVisibility(VISIBLE);
                 }
             }
-        }else if(mLoadingModel == MODEL_ALERT){
+        } else if (mLoadingModel == MODEL_ALERT) {
             setVisibility(GONE);
+            if (hasCustomLoadingView && mCustomLoadingView != null) {
+                mCustomLoadingView.setVisibility(VISIBLE);
+            }
             mProgressDialog.setCancelable(true);
-//            mProgressDialog.setCancelable(false);
             mProgressDialog.show();
         }
 
@@ -148,11 +150,11 @@ public class MyLoadingView extends RelativeLayout  implements View.OnClickListen
     /**
      * 加载成功
      */
-    public void success(){
-        if(mBindView!=null){
+    public void success() {
+        if (mBindView != null) {
             mBindView.setVisibility(VISIBLE);
         }
-        if(mLoadingModel == MODEL_ALERT){
+        if (mLoadingModel == MODEL_ALERT) {
             mProgressDialog.cancel();
         }
         setVisibility(GONE);
@@ -161,42 +163,48 @@ public class MyLoadingView extends RelativeLayout  implements View.OnClickListen
     /**
      * dismiss dialog
      */
-    public void dismiss(){
-        if (mProgressDialog!=null)
-        mProgressDialog.dismiss();
+    public void dismiss() {
+        if (mBindView != null) {
+            mBindView.setVisibility(VISIBLE);
+        }
+        if (mLoadingModel == MODEL_ALERT && mProgressDialog != null) {
+            mProgressDialog.dismiss();
+        }
+        setVisibility(GONE);
     }
 
     /**
      * 加载失败
      */
-    public void empty(){
+    public void empty() {
         empty("");
     }
 
     /**
      * 加载失败
+     *
      * @param msg 加载失败提示语
      */
-    public void empty(String msg){
+    public void empty(String msg) {
 
-        if(!TextUtils.isEmpty(msg)){
+        if (!TextUtils.isEmpty(msg)) {
             mWarnText = msg;
         }
 
-        if(mLoadingModel == MODEL_ALERT){
+        if (mLoadingModel == MODEL_ALERT) {
             mProgressDialog.cancel();
         }
 
-        if(mBindView!=null){
+        if (mBindView != null) {
             mBindView.setVisibility(GONE);
         }
         setVisibility(VISIBLE);
         mLoadDataBtn.setVisibility(VISIBLE);
 
-        if(!hasCustomLoadingView){
+        if (!hasCustomLoadingView) {
             mWarnView.setText(mWarnText);
-        }else{
-            if(mCustomLoadingView!=null){
+        } else {
+            if (mCustomLoadingView != null) {
                 mWarnView.setVisibility(VISIBLE);
                 mWarnView.setText(mWarnText);
                 mCustomLoadingView.setVisibility(GONE);
@@ -207,9 +215,10 @@ public class MyLoadingView extends RelativeLayout  implements View.OnClickListen
 
     /**
      * 设置绑定view
+     *
      * @param view
      */
-    public void bindView(View view){
+    public void bindView(View view) {
         this.mBindView = view;
     }
 
@@ -219,19 +228,20 @@ public class MyLoadingView extends RelativeLayout  implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
-        if(onBtnClickListener!=null){
+        if (onBtnClickListener != null) {
             onBtnClickListener.onBtnClick();
-        }else{
+        } else {
             throw new IllegalArgumentException("must be set click");
         }
     }
 
     /**
      * 设置自定义加载view
+     *
      * @param view
      */
-    public void setCustomLoadingView(View view){
-        if(view!=null){
+    public void setCustomLoadingView(View view) {
+        if (view != null) {
             mCustomLoadingView = view;
             hasCustomLoadingView = true;
 
@@ -247,21 +257,22 @@ public class MyLoadingView extends RelativeLayout  implements View.OnClickListen
 
     /**
      * 设置弹出框模式加载中文字
+     *
      * @param loadingText
      */
     public void setLoadingText(String loadingText) {
-        if(!TextUtils.isEmpty(loadingText)){
-            if(mProgressDialog!=null){
+        if (!TextUtils.isEmpty(loadingText)) {
+            if (mProgressDialog != null) {
 //                mProgressDialog.setLoadingText(loadingText);
             }
         }
     }
 
-    public void setLoadingModel(int model){
+    public void setLoadingModel(int model) {
         this.mLoadingModel = model;
     }
 
-    public interface OnBtnClickListener{
+    public interface OnBtnClickListener {
         /**
          * 重新加载回调接口
          */
