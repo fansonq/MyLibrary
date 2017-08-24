b7f93f425c5d169f33d25994e82eb898eba67d55
company\fanson <fansonq@gmail.com>
2017-08-24 14:09:13 +0800

    添加pullzoominview

---
+++ MyLibrary/fansonlib/src/main/java/com/example/fansonlib/utils/ServiceUtils.java
@@ -0,0 +1,37 @@
+package com.example.fansonlib.utils;
+
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.ArrayList;
+
+/**
+ * Created by：fanson
+ * Created on：2017/8/21 11:49
+ * Describe：服务的工具类
+ */
+
+public class ServiceUtils {
+
+
+    /**
+     * 判断服务是否在运行中
+     * @param context 即为Context对象
+     * @param serviceName 即为Service的全名(包名+服务的类名)
+     * @return 是否在运行中
+     */
+    private static boolean isServiceRunning(Context context, String serviceName) {
+        boolean isRun = false;
+        ActivityManager myManager = (ActivityManager) context
+                .getApplicationContext().getSystemService(
+                        Context.ACTIVITY_SERVICE);
+        ArrayList<ActivityManager.RunningServiceInfo> runningService = (ArrayList<ActivityManager.RunningServiceInfo>) myManager
+                .getRunningServices(100);
+        for (int i = 0; i < runningService.size(); i++) {
+            if (runningService.get(i).service.getClassName().equals(serviceName)) {
+                isRun = true;
+            }
+        }
+        return isRun;
+    }
+}
---
+++ MyLibrary/fansonlib/src/main/java/com/example/fansonlib/widget/pullzoomIn/IPullZoomIn.java
@@ -0,0 +1,56 @@
+package com.example.fansonlib.widget.pullzoomIn;
+
+import android.content.res.TypedArray;
+import android.view.View;
+
+/**
+ * Created by：fanson
+ * Created on：2017/8/24 13:54
+ * Describe：
+ */
+
+public interface IPullZoomIn<T extends View> {
+    /**
+     * Get the Wrapped Zoom View. Anything returned here has already been
+     * added to the content view.
+     *
+     * @return The View which is currently wrapped
+     */
+    public View getZoomView();
+
+    public View getHeaderView();
+
+    /**
+     * Get the Wrapped root View.
+     *
+     * @return The View which is currently wrapped
+     */
+    public T getPullRootView();
+
+    /**
+     * Whether Pull-to-Refresh is enabled
+     *
+     * @return enabled
+     */
+    public boolean isPullToZoomEnabled();
+
+    /**
+     * Returns whether the Widget is currently in the Zooming state
+     *
+     * @return true if the Widget is currently zooming
+     */
+    public boolean isZooming();
+
+    /**
+     * Returns whether the Widget is currently in the Zooming anim type
+     *
+     * @return true if the anim is parallax
+     */
+    public boolean isParallax();
+
+    public boolean isHideHeader();
+
+    public void handleStyledAttributes(TypedArray a);
+}
+
+
