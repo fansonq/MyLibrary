c429858ae52fda1b6e6add3da00aaabe87f48c5a
company\fanson <fansonq@gmail.com>
2017-09-30 13:46:43 +0800

    更改BaseHolder；

--- MyLibrary/fansonlib/src/main/java/com/example/fansonlib/base/BaseHolder.java
+++ MyLibrary/fansonlib/src/main/java/com/example/fansonlib/base/BaseHolder.java
@@ -67,7 +67,7 @@ public  class BaseHolder extends RecyclerView.ViewHolder {
      * @param <T>    View的类型
      * @return view
      */
-    protected <T extends View> T getView(@IdRes int viewId) {
+    public <T extends View> T getView(@IdRes int viewId) {
         View view = viewArray.get(viewId);
         if (view == null) {
             view = itemView.findViewById(viewId);
