d71773912052a4717419909396486c720b297f6b
fansonq <fansonq@gmail.com>
2018-10-16 17:26:56 +0800

    完善：DimensUtils工具类；

--- MyLibrary/fansonlib/src/main/java/com/example/fansonlib/utils/DimensUtils.java
+++ MyLibrary/fansonlib/src/main/java/com/example/fansonlib/utils/DimensUtils.java
@@ -1,6 +1,16 @@
 package com.example.fansonlib.utils;
 
+import android.app.Activity;
+import android.app.ActivityGroup;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
 
 /**
  * @author Created by：fanson
@@ -8,6 +18,9 @@ import android.content.Context;
  * Description：屏幕工具类
  */
 public class DimensUtils {
+
+    private static int mStatusHeight = -1;
+
     /**
      * dip转px
      */
@@ -32,21 +45,145 @@ public class DimensUtils {
     }
 
     /**
-     * 获取屏幕宽度（px）
+     * 获取屏幕高度（像素）
+     *
      * @param context 上下文
-     * @return 屏幕宽度（px）
+     * @return 屏幕高度（像素）
      */
-    public static int getScreenWidth(Context context){
-        return context.getResources().getDisplayMetrics().widthPixels;
+    public static int getHeightPixel(Context context) {
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        return dm.heightPixels;
     }
 
     /**
-     * 获取屏幕高度（px）
+     * 获取屏幕宽度（像素）
+     *
      * @param context 上下文
-     * @return 屏幕高度（px）
+     * @return 屏幕宽度（像素）
      */
-    public static int getScreenHeight(Context context){
-        return context.getResources().getDisplayMetrics().heightPixels;
+    public static int getWidthPixel(Context context) {
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        return dm.widthPixels;
     }
+
+    /**
+     * 获取屏幕高度（dp）
+     *
+     * @param context 上下文
+     * @return 屏幕高度（dp）
+     */
+    public static int getHeightDp(Context context) {
+        // 屏幕高度算法:屏幕高度（像素）/屏幕密度
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        return (int) (dm.heightPixels / dm.density);
+    }
+
+    /**
+     * 获取屏幕宽度（dp）
+     *
+     * @param context 上下文
+     * @return 屏幕宽度（dp）
+     */
+    public static int getWidthDp(Context context) {
+        // 屏幕宽度算法:屏幕宽度（像素）/屏幕密度
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        return (int) (dm.widthPixels / dm.density);
+
+    }
+
+    /**
+     * 获取屏幕密度
+     *
+     * @param context 上下文
+     * @return 屏幕密度(int)
+     */
+    public static int getDensity(Context context) {
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        return (int) dm.density;
+    }
+
+    /**
+     * 获得状态栏的高度
+     *
+     * @param context 上下文
+     * @return 状态栏高度
+     */
+    public static int getStatusHeight(Context context) {
+        if (mStatusHeight != -1) {
+            return mStatusHeight;
+        }
+        try {
+            int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+            if (resourceId > 0) {
+                mStatusHeight = context.getResources().getDimensionPixelSize(resourceId);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mStatusHeight;
+    }
+
+
+    /**
+     * 获取当前屏幕截图，不包含状态栏
+     *
+     * @param activity Activity
+     * @return 当前屏幕截图（不含状态栏）
+     */
+    public static Bitmap getSnapShotWithoutStatusBar(Activity activity) {
+        View view = activity.getWindow().getDecorView();
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache();
+        Bitmap bmp = view.getDrawingCache();
+        if (bmp == null) {
+            return null;
+        }
+        Rect frame = new Rect();
+        activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+        int statusBarHeight = frame.top;
+        Bitmap bp = Bitmap.createBitmap(bmp, 0, statusBarHeight, bmp.getWidth(), bmp.getHeight() - statusBarHeight);
+        view.destroyDrawingCache();
+        view.setDrawingCacheEnabled(false);
+        return bp;
+    }
+
+    /**
+     * 获取actionbar的像素高度，默认使用android官方兼容包做actionbar兼容
+     *
+     * @return
+     */
+    public static int getActionBarHeight(Context context) {
+        int actionBarHeight = 0;
+        if (context instanceof AppCompatActivity && ((AppCompatActivity) context).getSupportActionBar() != null) {
+            Log.d("isAppCompatActivity", "==AppCompatActivity");
+            actionBarHeight = ((AppCompatActivity) context).getSupportActionBar().getHeight();
+        } else if (context instanceof Activity && ((Activity) context).getActionBar() != null) {
+            Log.d("isActivity", "==Activity");
+            actionBarHeight = ((Activity) context).getActionBar().getHeight();
+        } else if (context instanceof ActivityGroup) {
+            Log.d("ActivityGroup", "==ActivityGroup");
+            if (((ActivityGroup) context).getCurrentActivity() instanceof AppCompatActivity && ((AppCompatActivity) ((ActivityGroup) context).getCurrentActivity()).getSupportActionBar() != null) {
+                actionBarHeight = ((AppCompatActivity) ((ActivityGroup) context).getCurrentActivity()).getSupportActionBar().getHeight();
+            } else if (((ActivityGroup) context).getCurrentActivity() instanceof Activity && ((Activity) ((ActivityGroup) context).getCurrentActivity()).getActionBar() != null) {
+                actionBarHeight = ((Activity) ((ActivityGroup) context).getCurrentActivity()).getActionBar().getHeight();
+            }
+        }
+        if (actionBarHeight != 0)
+            return actionBarHeight;
+        final TypedValue tv = new TypedValue();
+        if (context.getTheme().resolveAttribute(android.support.v7.appcompat.R.attr.actionBarSize, tv, true)) {
+            if (context.getTheme().resolveAttribute(android.support.v7.appcompat.R.attr.actionBarSize, tv, true))
+                actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, context.getResources().getDisplayMetrics());
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            if (context.getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true))
+                actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, context.getResources().getDisplayMetrics());
+        } else {
+            if (context.getTheme().resolveAttribute(android.support.v7.appcompat.R.attr.actionBarSize, tv, true))
+                actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, context.getResources().getDisplayMetrics());
+        }
+        Log.d("actionBarHeight", "====" + actionBarHeight);
+        return actionBarHeight;
+    }
+
 
 }
