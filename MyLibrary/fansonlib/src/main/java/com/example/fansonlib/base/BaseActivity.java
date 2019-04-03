1da5145a4cb44ce7fb15ddd63db7af93eeb7f08a
fansonq <fansonq@gmail.com>
2019-04-03 10:19:44 +0800

    完善BaseActivity；

--- MyLibrary/fansonlib/src/main/java/com/example/fansonlib/base/BaseActivity.java
+++ MyLibrary/fansonlib/src/main/java/com/example/fansonlib/base/BaseActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
@@ -259,6 +260,11 @@ public abstract class BaseActivity<D extends ViewDataBinding> extends AppCompatA
             default:
                 break;
         }
+    }
+
+
+    @Override
+    public void currentFragmentBack(Fragment fragment) {
     }
 
     @Override
