a6cdc17e6054a797ef3d7a9b79b72e2eed82e7f7
fansonq <fansonq@gmail.com>
2019-11-20 09:15:08 +0800

    修改AutoInstallHelper；

--- MyLibrary/fansonlib/src/main/java/com/example/fansonlib/utils/AutoInstallHelper.java
+++ MyLibrary/fansonlib/src/main/java/com/example/fansonlib/utils/AutoInstallHelper.java
@@ -23,17 +23,18 @@ public class AutoInstallHelper {
     /**
      * 开始安装（兼容Android7.0以上）
      *
-     * @param context 上下文
-     * @param file    安装的文件Apk
+     * @param context   上下文
+     * @param file      安装的文件Apk
+     * @param authority Provider地址
      */
-    public static void startInstall(Context context, File file) {
+    public static void startInstall(Context context, File file, String authority) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         //版本在7.0以上是不能直接通过uri访问的
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             // 由于没有在Activity环境下启动Activity,设置下面的标签
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             //参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
-            Uri apkUri = FileProvider.getUriForFile(context, context.getPackageName() + ".provider", file);
+            Uri apkUri = FileProvider.getUriForFile(context, authority, file);
             //添加这一句表示对目标应用临时授权该Uri所代表的文件
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
